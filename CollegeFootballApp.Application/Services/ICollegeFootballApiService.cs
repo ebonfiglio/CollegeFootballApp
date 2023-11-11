01fa361599177627c71212170a6ecbc3cdc9f237
Evan Bonfiglio <evanbonfiglio@gmail.com>
2023-11-11 13:34:05 -0500

    Add api service

---
+++ CollegeFootballApp.Application/Services/ICollegeFootballApiService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollegeFootballApp.Application.Services
+{
+    public interface ICollegeFootballApiService
+    {
+        Task GetFBSGames(int startYear, int endYear);
+    }
+}
---
+++ CollegeFootballApp.Infrastructure/Services/CollegeFootballApiService.cs
@@ -0,0 +1,29 @@
+﻿using CFBSharp.Api;
+using CFBSharp.Client;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollegeFootballApp.Infrastructure.Services
+{
+    internal class CollegeFootballApiService
+    {
+        private readonly GamesApi _gamesApi;
+
+        public CollegeFootballApiService()
+        {
+            // Initialize the API client
+            Configuration configuration = new() 
+            {
+                BasePath = "https://api.collegefootballdata.com",
+            };
+
+            configuration.AddApiKey("Authorization", "YOUR_API_KEY");
+
+            _gamesApi = new GamesApi(configuration);
+        }
+    }
+}
